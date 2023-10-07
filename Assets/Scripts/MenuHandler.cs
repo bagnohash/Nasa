d11378d908a63e6e4261bcaad3fc3dde10967e47
bagnohash <57256260+bagnohash@users.noreply.github.com>
2023-10-07 21:43:39 +0200

    dupa

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour
+{
+    public TextMeshProUGUI upperText;
+    public GameObject aboutPanel;
+
+    public GameObject playButton;
+    public GameObject aboutButton;
+    public GameObject quitButton;
+    public GameObject returnButton;
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+    public void About()
+    {
+        playButton.SetActive(false);
+        aboutButton.SetActive(false);
+        quitButton.SetActive(false);
+        
+        returnButton.SetActive(true);
+        aboutPanel.SetActive(true);
+    }
+
+    public void Return()
+    {
+        playButton.SetActive(true);
+        aboutButton.SetActive(true);
+        quitButton.SetActive(true);
+
+        returnButton.SetActive(false);
+        aboutPanel.SetActive(false);
+    }
+
+}
